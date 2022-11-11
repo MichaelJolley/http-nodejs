0438990a6eed7dfc48331f20699b636d9df0196f
Michael Jolley <mike@sparcapp.io>
2022-11-11 21:05:17 +0000

    test

--- server.mjs
+++ server.mjs
@@ -4,6 +4,18 @@ import pg from 'pg';
 
 
 createServer(async (req, res) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+    /** add other headers as per requirement */
+  };
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
 
   const url = req.url;
 
