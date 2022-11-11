e9f0ad4ebdb16dc4af238f71b3472ead86613d65
Michael Jolley <mike@sparcapp.io>
2022-11-11 20:35:03 +0000

    test

--- server.mjs
+++ server.mjs
@@ -23,6 +23,8 @@ createServer(async (req, res) => {
     const pokeEntry = await client.query('SELECT * from pokemon where name=$1', [poke]);
     const pokemon = pokeEntry.rows[0];
 
+    console.log(pokemon);
+
     const evolvedEntries = await client.query('SELECT * from pokemon where evolved_to=$1', [pokemon.id]);
     await client.end();
 
