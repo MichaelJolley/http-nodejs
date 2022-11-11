5d1cbb4b9354f2060744f225c42ff773ad8f5ea4
Michael Jolley <mike@sparcapp.io>
2022-11-11 20:27:20 +0000

    Updating server

--- server.mjs
+++ server.mjs
@@ -1,33 +1,33 @@
 import { createServer } from 'http';
-import { Client} from 'pg';
+import { Client } from 'pg';
 
 const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    ssl: true,
-  });
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  ssl: true,
+});
 
 createServer(async (req, res) => {
-  
+
   const url = req.url;
-  
+
   if (url.startsWith('/pokemon')) {
-    res.writeHead(200);
-    
-    const poke = url.replace('/pokemon/','');
-    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const poke = url.replace('/pokemon/', '');
+
     await client.connect();
     const pokeEntry = await client.query('SELECT * from pokemon where name=$1', [poke]);
-    const pokemon = pokeEntry.rows?[0];
-    
+    const pokemon = pokeEntry.rows[0];
+
     const evolvedEntries = await client.query('SELECT * from pokemon where evolved_to=$1', [pokemon.id]);
     await client.end();
-    
-    res.end(JSON.stringify(evolvedEntries));
+
+    res.end(JSON.stringify(evolvedEntries.rows));
   }
-  
+
   res.end();
 }).listen(process.env.PORT);
